c382b0502e43ab096e14d1132461f6900a10e91f
suttikhun sihsobhon <suttikhun.sihsobhon@lseg.com>
2023-07-14 22:53:25 +0700

    prod env api adjust

--- local-issue-updater-prototype/src/app/utils/apiHelper.ts
+++ local-issue-updater-prototype/src/app/utils/apiHelper.ts
@@ -1,54 +1,36 @@
-// prod test
-// import { google } from "googleapis";
-// import { promises as fsp } from "fs"
-// import { GoogleAuthOptions } from "google-auth-library";
-// import { values } from "lodash";
+import { promises as fsp } from "fs"
+import { google } from "googleapis";
+import { GoogleAuthOptions } from "google-auth-library";
+import { values, map, slice ,replace,get } from "lodash";
 
-/**@todo use generated key file on production (GOOGLE_SHEET_KEYFILE_PATH) instead of the one from local secrets */
-// const GOOGLE_SHEET_KEYFILE_PATH = '/tmp/googleSheetKeyFile.json'
-// const SHEET_RANGE_ADD = 'MainPage!A1:G' // used to append a record
 
-// // This funtion is to connect googlesheet api
-// const connectGoogleSheetsApi = async () => {
-//     let auth;
-//     const googleSheetAuthConfig = await getGoogleSheetAuthConfig();
-//     try {
-//         auth = await google.auth.getClient(googleSheetAuthConfig)
-//     } catch (error) {
-//         throw new Error(error as string);
-//     }
-//     // connect to googlesheet 
-//     const sheets = google.sheets({ version: 'v4', auth })
-//     return sheets
-// }
+const singleObjJsonFileGenerator = async (obj: Object, path: string) => {
+    const json = JSON.stringify(obj);
+    try {
+        await fsp.writeFile(path, json);
+    } catch (error) {
+        throw new Error(error as string)
+    }
+}
 
-// const singleObjJsonFileGenerator = async (obj: Object, path: string) => {
-//     const json = JSON.stringify(obj);
-//     try {
-//         await fsp.writeFile(path, json);
-//     } catch (error) {
-//         throw new Error(error as string)
-//     }
-// }
-
-// const getGoogleSheetAuthConfig = async () => {
-//     const ggSheetCredential = JSON.parse(
-//         Buffer.from(process.env.GG_SHEET_KEY_BASE64 || '', "base64").toString()
-//     );
-//     //generate a json file to store a keyfile
-//     await singleObjJsonFileGenerator(ggSheetCredential, GOOGLE_SHEET_KEYFILE_PATH);
-//     // google sheets
-//     const GOOGLE_SHEET_AUTH_CONFIG: GoogleAuthOptions = {
-//         //scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
-//         scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-//         /**
-//          * @note keyfile from secrets folder local test.
-//          * Use keyfile from generated one instead on the production with keys from the env vars
-//         */
-//         keyFile: GOOGLE_SHEET_KEYFILE_PATH,
-//     }
-//     return GOOGLE_SHEET_AUTH_CONFIG;
-// }
+const getGoogleSheetAuthConfig = async () => {
+    const ggSheetCredential = JSON.parse(
+        Buffer.from(process.env.GG_SHEET_KEY_BASE64 || '', "base64").toString()
+    );
+    //generate a json file to store a keyfile
+    await singleObjJsonFileGenerator(ggSheetCredential, '/tmp/googleSheetKeyFile.json');
+    // google sheets
+    const GOOGLE_SHEET_AUTH_CONFIG: GoogleAuthOptions = {
+        //scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+        /**
+         * @note keyfile from secrets folder local test.
+         * Use keyfile from generated one instead on the production with keys from the env vars
+        */
+        keyFile: '/tmp/googleSheetKeyFile.json',
+    }
+    return GOOGLE_SHEET_AUTH_CONFIG;
+}
 
 // const transformToArrayTobeAddedToGGSheet = (formData: Object, expectedLength: number) => {
 //     if (Object.keys(formData).length !== expectedLength) {
@@ -145,9 +127,6 @@
 
 
 // local env
-import { google } from "googleapis";
-import { GoogleAuthOptions } from "google-auth-library";
-import { values, map, slice ,replace,get } from "lodash";
 
 const SHEET_RANGE_ADD = 'test-issues!A1:L' // used to append a record
 
@@ -163,15 +142,6 @@ const connectGoogleSheetsApi = async () => {
     // connect to googlesheet 
     const sheets = google.sheets({ version: 'v4', auth })
     return sheets
-}
-
-const getGoogleSheetAuthConfig = async () => {
- 
-    const GOOGLE_SHEET_AUTH_CONFIG: GoogleAuthOptions = {
-        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-        keyFile: './secrets/googleSheetKeyFile.json',
-    }
-    return GOOGLE_SHEET_AUTH_CONFIG;
 }
 
 const transformToArrayTobeAddedToGGSheet = (formData: Object, expectedLength: number) => {

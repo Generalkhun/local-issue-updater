e30c2ef1542a022dfbec742b51028167b0aff9b0
suttikhun sihsobhon <suttikhun.sihsobhon@lseg.com>
2023-07-30 16:25:52 +0700

    d

--- local-issue-updater-prototype/src/app/api/getIssuesData/route.ts
+++ local-issue-updater-prototype/src/app/api/getIssuesData/route.ts
@@ -1,6 +1,7 @@
 
 import { getIssuesDataFromGGSheet } from '@/app/utils/apiHelper'
 import { NextRequest, NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 
 export async function GET(req: NextRequest) {
     // add data to sheet
